4836d04da4af19be2dfb198fdc5c581f4e7f0ae9
annuanki <chen__hb@163.com>
2019-02-15 19:47:43 +0800

    chen

--- src/domains/ne/cartpoles/policy.py
+++ src/domains/ne/cartpoles/policy.py
@@ -1,197 +1,208 @@
-#!/usr/bin/python
-# -*- coding: UTF-8 -*-
-
-import numpy as np
 import tensorflow as tf
-import gym
+import numpy as np
+
 import os
 import csv
+import copy
 from domains.ne.cartpoles.enviornment.cartpole import SingleCartPoleEnv
-import domains.ne.cartpoles.enviornment.runner as runner
 from domains.ne.cartpoles.enviornment import force
 
-batch_size = 2
-learning_rate = 1e-1
-
-class PolicyNet:
-    def __init__(self):
-
-
-        # 输入层
-        self.input_dimension = 4
-        self.var_input_x = tf.placeholder(tf.float32, [None, self.input_dimension], name="input_x")
-        # 隐藏层１(50个神经元+Relu)
-        self.H1 = 50
-        self.W1 = tf.get_variable("w1", shape=[self.input_dimension, self.H1],
-                     initializer=tf.contrib.layers.xavier_initializer())
-        self.L1 = tf.nn.relu(tf.matmul(self.var_input_x, self.W1))
-        # 输出层
-        self.W2 = tf.get_variable("w2", shape=[self.H1, 1],
-                     initializer=tf.contrib.layers.xavier_initializer())
-        self.L2 = tf.matmul(self.L1, self.W2)
-
-        self.var_output = tf.nn.sigmoid(self.L2)# 根据概率来求损失和梯度
-
-        self.var_input_y = tf.placeholder(tf.float32, [None, 1], name="input_y")
-        self.var_reward  = tf.placeholder(tf.float32, name="reward")
-
-        self.loglik = tf.log(self.var_input_y * (self.var_input_y - self.var_output) +
-                (1 - self.var_input_y) * (self.var_input_y + self.var_output))
-        self.loss = -tf.reduce_mean(self.loglik * self.var_reward )
-
-        self.tvars = tf.trainable_variables()
-        self.newGrads = tf.gradients(self.loss, self.tvars)# 根据梯度优化训练两层神经网络
-        self.learning_rate = 1e-1
-        self.adam = tf.train.AdamOptimizer(learning_rate=learning_rate)
-        self.W1grad = tf.placeholder(tf.float32, name="batch_grad1")
-        self.W2grad = tf.placeholder(tf.float32, name="batch_grad2")
-        self.batchGrad = [self.W1grad,self.W2grad]
-        self.updateGrads = self.adam.apply_gradients(zip(self.batchGrad, self.tvars))
-
-    def getCorrectAction(self,x):
-        theta = x[0][2]
-        return 0 if theta > 0 else 1
+# 引用自：https://github.com/simoninithomas/Deep_reinforcement_learning_Course/blob/master/Policy%20Gradients/Cartpole/Cartpole%20REINFORCE%20Monte%20Carlo%20Policy%20Gradients.ipynb
 
 env = SingleCartPoleEnv().unwrapped
-net = PolicyNet()
+
+## ENVIRONMENT Hyperparameters
+state_size = 4
+action_size = env.action_space.n
+
+## TRAINING Hyperparameters
+learning_rate = 0.01
+gamma = 0.95 # Discount rate
+
 
 mode = 'noreset'
 maxepochcount = 1000
 complexunit = 20.
 
-def _dountilpoledown(session):
-    # 反复执行直到杆子倒下
-    notdone_count = 0
-    input_x_records = []
-    input_y_records = []
-    reward_records = []
-    step = 0
-    observation = env.reset()
-    while True:
-        # 执行一次
-        x = np.reshape(observation, [1, net.input_dimension])
-        tfprob = session.run(net.var_output, feed_dict={net.var_input_x: x})
-        action = 1 if np.random.uniform() < tfprob else 0
-        input_x_records.append(x)
-        input_y_records.append(net.getCorrectAction(x))
-        env.wind = force.force_generator.next(step * env.tau)
-        observation, reward, done, info = env.step(action)
+class PolicyGradients:
+    def __init__(self):
+        with tf.name_scope("inputs"):
+            self.input_ = tf.placeholder(tf.float32, [None, state_size], name="input_")
+            self.actions = tf.placeholder(tf.int32, [None, action_size], name="actions")
+            self.discounted_episode_rewards_ = tf.placeholder(tf.float32, [None, ], name="discounted_episode_rewards")
 
-        if not done:
-            notdone_count += 1
-            if notdone_count >= env.max_notdone_count:
-                break
-            step += 1
-            continue
-        break
-    reward_records = [1.0 / len(input_x_records) for t in input_x_records]
-    return notdone_count,input_x_records,input_y_records,reward_records,step
+            # Add this placeholder for having this variable in tensorboard
+            self.mean_reward_ = tf.placeholder(tf.float32, name="mean_reward")
 
+            with tf.name_scope("fc1"):
+                fc1 = tf.contrib.layers.fully_connected(inputs=self.input_,
+                                                        num_outputs=10,
+                                                        activation_fn=tf.nn.relu,
+                                                        weights_initializer=tf.contrib.layers.xavier_initializer())
 
+            with tf.name_scope("fc2"):
+                fc2 = tf.contrib.layers.fully_connected(inputs=fc1,
+                                                        num_outputs=action_size,
+                                                        activation_fn=tf.nn.relu,
+                                                        weights_initializer=tf.contrib.layers.xavier_initializer())
 
-def run(**kwargs):
-    global mode
-    global maxepochcount
-    global complexunit
-    global env
-    global net
+            with tf.name_scope("fc3"):
+                fc3 = tf.contrib.layers.fully_connected(inputs=fc2,
+                                                        num_outputs=action_size,
+                                                        activation_fn=None,
+                                                        weights_initializer=tf.contrib.layers.xavier_initializer())
 
-    mode = 'noreset' if 'mode' not in kwargs.keys() else kwargs['mode']
-    maxepochcount = 1000 if 'maxepochcount' not in kwargs.keys() else int(kwargs['maxepochcount'])
-    complexunit = 20.0 if 'complexunit' not in kwargs.keys() else float(kwargs['complexunit'])
-    xh = None if 'xh' not in kwargs.keys() else int(kwargs['xh'])
+            with tf.name_scope("softmax"):
+                self.action_distribution = tf.nn.softmax(fc3)
 
-    execute(xh,mode)
+            with tf.name_scope("loss"):
+                # tf.nn.softmax_cross_entropy_with_logits computes the cross entropy of the result after applying the softmax function
+                # If you have single-class labels, where an object can only belong to one class, you might now consider using
+                # tf.nn.sparse_softmax_cross_entropy_with_logits so that you don't have to convert your labels to a dense one-hot array.
+                self.neg_log_prob = tf.nn.softmax_cross_entropy_with_logits_v2(logits=fc3, labels=self.actions)
+                self.loss = tf.reduce_mean(self.neg_log_prob * self.discounted_episode_rewards_)
 
-def execute(xh=None,mode='noreset'):
-    global env
-    global  net
-    complexes = []
-    notdone_count_list = []
+            with tf.name_scope("train"):
+                self.train_opt = tf.train.AdamOptimizer(learning_rate).minimize(self.loss)
 
-    # 初始化
-    session = tf.Session()
-    init = tf.global_variables_initializer()
-    session.run(init)
-    observation = env.reset()
-    gradBuff = session.run(net.tvars)
-    for ix, grad in enumerate(gradBuff):
-        gradBuff[ix] = grad * 0
+    def _dounitdown(self,sess):
+        '''
+        执行直到杆子倒下
+        :param sess:
+        :return:
+        '''
+        state = env.reset()
+        episode_states = []
+        episode_actions = []
+        not_downcount = 0
 
-    episode_number = 0
-    episode_notdone_count_list = []
+        while True:
 
-    if xh is None or str(int(xh)) == '':
-        xh = ''
-    else:
-        xh = "_" + str(int(xh))
-    while True:
-        # 执行一次
-        notdone_count,input_x_records,input_y_records,reward_records,step = _dountilpoledown(session)
-        episode_notdone_count_list.append(notdone_count)
-        # 计算新梯度
-        tGrad = session.run(net.newGrads, feed_dict={net.var_input_x: np.vstack(input_x_records),
-                                                  net.var_input_y: np.vstack(input_y_records),
-                                                  net.var_reward: np.vstack(reward_records)})
-        for ix, grad in enumerate(tGrad):
-            gradBuff[ix] += grad
-        # 更新梯度
-        episode_number += 1
-        if episode_number % batch_size == 0:
-            session.run(net.updateGrads, feed_dict={net.W1grad: gradBuff[0],
-                                             net.W2grad: gradBuff[1]})
-            for ix, grad in enumerate(gradBuff):
-                gradBuff[ix] = grad * 0
+            # 执行一次
+            action_probability_distribution = sess.run(self.action_distribution,
+                                                       feed_dict={self.input_: state.reshape([1, 4])})
 
-        #if episode_number % 100 == 0 and episode_number != 0:
-        #    print("持续次数=", episode_notdone_count_list, ",平均=", np.average(episode_notdone_count_list))
+            action = np.random.choice(range(action_probability_distribution.shape[1]),
+                                      p=action_probability_distribution.ravel())  # select action w.r.t the actions prob
 
-        if notdone_count > env.max_notdone_count or episode_number >= maxepochcount:
-            # 记录复杂度和对应获得的奖励
-            complexes.append(force.force_generator.currentComplex())
-            notdone_count_list.append(np.max(episode_notdone_count_list))
-            #filename = os.path.split(os.path.realpath(__file__))[0] + '\\datas\\policy' + str(xh) + '.npy'
-            #np.save(filename, (complexes, notdone_count_list))
-            print([(f, c) for f, c in zip(complexes, notdone_count_list)])
+            # Perform a
+            new_state, reward, done, info = env.step(action)
 
-            # 记录过程记录
-            filename = os.path.split(os.path.realpath(__file__))[0] + os.sep + 'datas_' + mode + os.sep + \
-                       'policy' + os.sep + 'policy' + str(xh) + '.csv'
-            out = open(filename, 'a', newline='')
-            csv_write = csv.writer(out, dialect='excel')
-            csv_write.writerow([complexes[-1]]+episode_notdone_count_list)
-            episode_notdone_count_list = []
+            # Store s, a, r
+            episode_states.append(state)
 
-            # 升级复杂度,为了加快执行速度,让复杂度增加幅度至少大于min_up
-            changed, newcomplex, k, w, f, sigma = force.force_generator.promptComplex(complexunit)
-            if not changed or newcomplex is None:
-                break  # 复杂度已经达到最大,结束
-            print('新的环境复杂度=%.3f,k=%.2f,w=%.2f,f=%.2f,sigma=%.2f' % (newcomplex, k, w, f, sigma))
-            #session.close()
-            #session = tf.Session()
-            #init = tf.global_variables_initializer()
-            #session.run(init)
-            #observation = env.reset()
-            #gradBuff = session.run(net.tvars)
-            #for ix, grad in enumerate(gradBuff):
-            #    gradBuff[ix] = grad * 0
+            # For actions because we output only one (the index) we need 2 (1 is for the action taken)
+            # We need [0., 1.] (if we take right) not just the index
+            action_ = np.zeros(action_size)
+            action_[action] = 1
 
-            episode_number = 0
-            #episode_notdone_count_list = []
-            if mode == 'reset':
-                env = SingleCartPoleEnv().unwrapped
-                net = PolicyNet()
+            episode_actions.append(action_)
+            #episode_rewards.append(reward)
 
-    tf.reset_default_graph()
-    session.close()
+            if not done:
+                not_downcount += 1
+                state = new_state
+                continue
 
+            return not_downcount,episode_states,episode_actions
+
+    def discount_and_normalize_rewards(self,episode_rewards):
+        '''
+        计算伽马折扣奖励
+        :return:
+        '''
+        discounted_episode_rewards = np.zeros_like(episode_rewards)
+        cumulative = 0.0
+        for i in reversed(range(len(episode_rewards))):
+            cumulative = cumulative * gamma + episode_rewards[i]
+            discounted_episode_rewards[i] = cumulative
+
+        mean = np.mean(discounted_episode_rewards)
+        std = np.std(discounted_episode_rewards)
+        discounted_episode_rewards = (discounted_episode_rewards - mean) / (std)
+
+        return discounted_episode_rewards
+
+    def run(self,**kwargs):
+        global mode
+        global maxepochcount
+        global complexunit
+        global env
+
+        mode = 'noreset' if 'mode' not in kwargs.keys() else kwargs['mode']
+        maxepochcount = 1000 if 'maxepochcount' not in kwargs.keys() else int(kwargs['maxepochcount'])
+        complexunit = 20.0 if 'complexunit' not in kwargs.keys() else float(kwargs['complexunit'])
+        xh = None if 'xh' not in kwargs.keys() else int(kwargs['xh'])
+
+        complexes_list = []
+        notdone_count_list = []
+        episode_number = 0
+        saver = tf.train.Saver()
+
+        if xh is None or str(int(xh)) == '':
+            xh = ''
+        else:
+            xh = "_" + str(int(xh))
+
+        sess = tf.Session()
+        sess.run(tf.global_variables_initializer())
+
+        while 1:
+            not_downcount,episode_states,episode_actions = self._dounitdown(sess)
+            episode_rewards = [1. for item in range(not_downcount+1)]
+            discounted_episode_rewards = self.discount_and_normalize_rewards(episode_rewards)
+
+            # Feedforward, gradient and backpropagation
+            loss_, _ = sess.run([self.loss, self.train_opt],
+                                feed_dict={self.input_: np.vstack(np.array(episode_states)),
+                                            self.actions: np.vstack(np.array(episode_actions)),
+                                            self.discounted_episode_rewards_: discounted_episode_rewards
+                                            })
+            notdone_count_list.append(not_downcount)
+            episode_number += 1
+
+            if not_downcount > env.max_notdone_count or episode_number >= maxepochcount:
+                complexes_list.append(force.force_generator.currentComplex())
+
+                print([(f, c) for f, c in zip(complexes_list, notdone_count_list)])
+
+                # 记录过程记录
+                filename = os.path.split(os.path.realpath(__file__))[0] + os.sep + 'datas_' + mode + os.sep + \
+                           'policy' + os.sep + 'policy' + str(xh) + '.csv'
+                out = open(filename, 'a', newline='')
+                csv_write = csv.writer(out, dialect='excel')
+                csv_write.writerow([complexes_list[-1]] + notdone_count_list)
+                notdone_count_list = []
+
+                # 升级复杂度,为了加快执行速度,让复杂度增加幅度至少大于min_up
+                changed, newcomplex, k, w, f, sigma = force.force_generator.promptComplex(complexunit)
+                if not changed or newcomplex is None or newcomplex == complexes_list[-1]:
+                    sess.close()
+                    return False
+                    break  # 复杂度已经达到最大,结束
+                print('新的环境复杂度=%.3f,k=%.2f,w=%.2f,f=%.2f,sigma=%.2f' % (newcomplex, k, w, f, sigma))
+
+                if mode == 'reset':
+                    sess.close()
+                    return True
+
+                episode_number = 0
+
+        sess.close()
 
 if __name__ == '__main__':
     force.init()
 
     for i in range(10):
-        run(mode='noreset', maxepochcount=2000, complexunit=20.,xh=i)
+        kwargs = {'mode': 'reset', 'xh':i,'maxepochcount' : 500,'complexunit':100.0}
         env = SingleCartPoleEnv().unwrapped
-        net = PolicyNet()
-        #force.force_generator = force.ForceGenerator(0.0, 0.0, 0.0, 1.01)
-
+        net = PolicyGradients()
+        while 1:
+            result = net.run(**kwargs)
+            if kwargs['mode'] == 'noreset':
+                break
+            if not result:
+                break
+            env = SingleCartPoleEnv().unwrapped
+            net = PolicyGradients()
+        force.force_generator = force.ForceGenerator(0.0, 0.0, 0.0, 1.01)
---
+++ src/domains/ne/cartpoles/policy3.py
@@ -0,0 +1,180 @@
+import tensorflow as tf
+import numpy as np
+
+import os
+import csv
+import copy
+from domains.ne.cartpoles.enviornment.cartpole import SingleCartPoleEnv
+from domains.ne.cartpoles.enviornment import force
+
+# 引用自：https://github.com/simoninithomas/Deep_reinforcement_learning_Course/blob/master/Policy%20Gradients/Cartpole/Cartpole%20REINFORCE%20Monte%20Carlo%20Policy%20Gradients.ipynb
+
+env = SingleCartPoleEnv().unwrapped
+
+## ENVIRONMENT Hyperparameters
+state_size = 4
+action_size = env.action_space.n
+
+## TRAINING Hyperparameters
+max_episodes = 300
+learning_rate = 0.01
+gamma = 0.95 # Discount rate
+
+
+def discount_and_normalize_rewards(episode_rewards):
+    discounted_episode_rewards = np.zeros_like(episode_rewards)
+    cumulative = 0.0
+    for i in reversed(range(len(episode_rewards))):
+        cumulative = cumulative * gamma + episode_rewards[i]
+        discounted_episode_rewards[i] = cumulative
+
+    mean = np.mean(discounted_episode_rewards)
+    std = np.std(discounted_episode_rewards)
+    discounted_episode_rewards = (discounted_episode_rewards - mean) / (std)
+
+    return discounted_episode_rewards
+
+class PolicyGradients:
+    def __init__(self):
+        with tf.name_scope("inputs"):
+            self.input_ = tf.placeholder(tf.float32, [None, state_size], name="input_")
+            self.actions = tf.placeholder(tf.int32, [None, action_size], name="actions")
+            self.discounted_episode_rewards_ = tf.placeholder(tf.float32, [None, ], name="discounted_episode_rewards")
+
+            # Add this placeholder for having this variable in tensorboard
+            self.mean_reward_ = tf.placeholder(tf.float32, name="mean_reward")
+
+            with tf.name_scope("fc1"):
+                fc1 = tf.contrib.layers.fully_connected(inputs=self.input_,
+                                                        num_outputs=10,
+                                                        activation_fn=tf.nn.relu,
+                                                        weights_initializer=tf.contrib.layers.xavier_initializer())
+
+            with tf.name_scope("fc2"):
+                fc2 = tf.contrib.layers.fully_connected(inputs=fc1,
+                                                        num_outputs=action_size,
+                                                        activation_fn=tf.nn.relu,
+                                                        weights_initializer=tf.contrib.layers.xavier_initializer())
+
+            with tf.name_scope("fc3"):
+                fc3 = tf.contrib.layers.fully_connected(inputs=fc2,
+                                                        num_outputs=action_size,
+                                                        activation_fn=None,
+                                                        weights_initializer=tf.contrib.layers.xavier_initializer())
+
+            with tf.name_scope("softmax"):
+                self.action_distribution = tf.nn.softmax(fc3)
+
+            with tf.name_scope("loss"):
+                # tf.nn.softmax_cross_entropy_with_logits computes the cross entropy of the result after applying the softmax function
+                # If you have single-class labels, where an object can only belong to one class, you might now consider using
+                # tf.nn.sparse_softmax_cross_entropy_with_logits so that you don't have to convert your labels to a dense one-hot array.
+                self.neg_log_prob = tf.nn.softmax_cross_entropy_with_logits_v2(logits=fc3, labels=self.actions)
+                self.loss = tf.reduce_mean(self.neg_log_prob * self.discounted_episode_rewards_)
+
+            with tf.name_scope("train"):
+                self.train_opt = tf.train.AdamOptimizer(learning_rate).minimize(self.loss)
+
+    def run(self,xh=None,mode='noreset'):
+        allRewards = []
+        total_rewards = 0
+        maximumRewardRecorded = 0
+        episode = 0
+        episode_states, episode_actions, episode_rewards = [], [], []
+
+        saver = tf.train.Saver()
+
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+
+            for episode in range(max_episodes):
+
+                episode_rewards_sum = 0
+
+                # Launch the game
+                state = env.reset()
+
+                #env.render()
+
+                while True:
+
+                    # Choose action a, remember WE'RE NOT IN A DETERMINISTIC ENVIRONMENT, WE'RE OUTPUT PROBABILITIES.
+                    action_probability_distribution = sess.run(self.action_distribution,
+                                                               feed_dict={self.input_: state.reshape([1, 4])})
+
+                    action = np.random.choice(range(action_probability_distribution.shape[1]),
+                                              p=action_probability_distribution.ravel())  # select action w.r.t the actions prob
+
+                    # Perform a
+                    new_state, reward, done, info = env.step(action)
+
+                    # Store s, a, r
+                    episode_states.append(state)
+
+                    # For actions because we output only one (the index) we need 2 (1 is for the action taken)
+                    # We need [0., 1.] (if we take right) not just the index
+                    action_ = np.zeros(action_size)
+                    action_[action] = 1
+
+                    episode_actions.append(action_)
+
+                    episode_rewards.append(reward)
+                    if done:
+
+                        # Calculate sum reward
+                        '''
+                        episode_rewards_sum = np.sum(episode_rewards)
+
+                        allRewards.append(episode_rewards_sum)
+
+                        total_rewards = np.sum(allRewards)
+
+                        # Mean reward
+                        mean_reward = np.divide(total_rewards, episode + 1)
+
+                        maximumRewardRecorded = np.amax(allRewards)
+
+                        print("==========================================")
+                        print("Episode: ", episode)
+                        print("Reward: ", episode_rewards_sum)
+                        print("Mean Reward", mean_reward)
+                        print("Max reward so far: ", maximumRewardRecorded)
+                        '''
+                        # Calculate discounted reward
+                        discounted_episode_rewards = discount_and_normalize_rewards(episode_rewards)
+
+                        # Feedforward, gradient and backpropagation
+                        loss_, _ = sess.run([self.loss, self.train_opt], feed_dict={self.input_: np.vstack(np.array(episode_states)),
+                                                                          self.actions: np.vstack(np.array(episode_actions)),
+                                                                          self.discounted_episode_rewards_: discounted_episode_rewards
+                                                                          })
+
+
+                        # Write TF Summaries
+                        '''
+                        summary = sess.run(write_op, feed_dict={self.input_: np.vstack(np.array(episode_states)),
+                                                                self.actions: np.vstack(np.array(episode_actions)),
+                                                                self.discounted_episode_rewards_: discounted_episode_rewards,
+                                                                self.mean_reward_: mean_reward
+                                                                })
+
+                        writer.add_summary(summary, episode)
+                        writer.flush()
+                        '''
+
+                        # Reset the transition stores
+                        episode_states, episode_actions, episode_rewards = [], [], []
+
+                        break
+
+                    state = new_state
+
+                # Save Model
+                if episode % 100 == 0:
+                    saver.save(sess, "./models/model.ckpt")
+                    print("Model saved")
+
+
+if __name__ == '__main__':
+    net = PolicyGradients()
+    net.run()
